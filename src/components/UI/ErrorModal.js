3545c75c6848df42c31e4817c4e2529a7a1d1fa5
Hafhis Gustiawan <hafhisgustiawan@gmail.com>
2023-10-01 21:29:44 +0700

    features adding portal react

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,35 +1,61 @@
+import { Fragment } from 'react';
+import ReactDOM from 'react-dom';
 import styles from './ErrorModal.module.css';
 
 import Button from './Button';
 import Card from './Card';
 
+const Backdrop = (props) => {
+  return (
+    <div
+      className={styles.backdrop}
+      onClick={() => {
+        props.onCloseModal();
+      }}
+    />
+  );
+};
+
+const Overlay = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button
+          onClick={() => {
+            props.onCloseModal();
+          }}
+        >
+          Okay
+        </Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = (props) => {
   return (
-    <div>
-      <div
-        className={styles.backdrop}
-        onClick={() => {
-          props.onCloseModal();
-        }}
-      />
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button
-            onClick={() => {
-              props.onCloseModal();
-            }}
-          >
-            Okay
-          </Button>
-        </footer>
-      </Card>
-    </div>
+    <Fragment>
+      {/*portal ini cek video 138, intinya dia inject html ke public/index.html*/}
+      {ReactDOM.createPortal(
+        <Backdrop onCloseModal={props.onCloseModal} />,
+        document.getElementById('backdrop-root')
+      )}
+
+      {ReactDOM.createPortal(
+        <Overlay
+          title={props.title}
+          message={props.message}
+          onCloseModal={props.onCloseModal}
+        />,
+        document.getElementById('overlay-root')
+      )}
+    </Fragment>
   );
 };
 
